2a159c3ff3d85c245eaadb81f47415c6fd26c241
anwarachoy <anwar.achoy@gmail.com>
2018-10-10 14:30:59 +0700

    Create mega1.ino

---
+++ MegaWifiMQTT/mega1.ino
@@ -0,0 +1,140 @@
+#include "WiFiEsp.h"
+#include "WiFiEspUdp.h"
+#include <PubSubClient.h>
+#include <WiFiEspClient.h>
+
+int counter = 0;
+char buff[30];
+
+IPAddress ip(123, 45, 0, 25);
+IPAddress server(192, 168, 0, 100);
+
+char ssid[] = "coba";       // your network SSID (name)
+char pass[] = "1234567890";       // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+// Initialize the Ethernet client object
+WiFiEspClient espClient;
+
+PubSubClient client(espClient);
+
+//print any message received for subscribed topic
+void callback(char* topic, byte* payload, unsigned int length) {
+  
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  delay(1);
+  client.publish("MainControl","active");
+  delay(1);
+  client.publish("1pulse_spc","1900");
+  delay(1);
+  sprintf(buff,"Syergie %d",counter);
+  client.publish("DEF",buff);
+  counter++;
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect, just a name to identify the client
+    if (client.connect("arduinoClient1")) {
+      Serial.println("connected");
+      sprintf(buff,"Syergie %d",counter);
+      client.publish("DEF",buff);
+      counter++;
+      client.subscribe("ABC");
+      // Once connected, publish an announcement...
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(1500);
+    }
+  }
+}
+
+void printWifiData()
+{
+  // print your WiFi shield's IP address
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print your MAC address
+  byte mac[6];
+  WiFi.macAddress(mac);
+  char buf[20];
+  sprintf(buf, "%02X:%02X:%02X:%02X:%02X:%02X", mac[5], mac[4], mac[3], mac[2], mac[1], mac[0]);
+  Serial.print("MAC address: ");
+  Serial.println(buf);
+}
+
+void printCurrentNet()
+{
+  // print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  char buf[20];
+  sprintf(buf, "%02X:%02X:%02X:%02X:%02X:%02X", bssid[5], bssid[4], bssid[3], bssid[2], bssid[1], bssid[0]);
+  Serial.print("BSSID: ");
+  Serial.println(buf);
+
+  // print the received signal strength
+  long rssi = WiFi.RSSI();
+  Serial.print("Signal strength (RSSI): ");
+  Serial.println(rssi);
+}
+
+void setup()
+{
+  // initialize serial for debugging
+  Serial.begin(115200);
+  // initialize serial for ESP module
+  Serial3.begin(9600);
+  // initialize ESP module
+  WiFi.init(&Serial3);
+
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(ssid, pass);
+    printCurrentNet();
+    printWifiData();
+  }
+
+  // you're connected now, so print out the data
+  Serial.println("You're connected to the network");
+
+  //connect to MQTT server
+  client.setServer(server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (!client.connected()) {
+    reconnect();
+  }
+  delay(5);
+  client.loop();
+  
+}
