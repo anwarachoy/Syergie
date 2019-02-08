e6b102d4dd64bc2982d2eb3545a60d3cc438c0e5
anwarachoy <anwar.achoy@gmail.com>
2019-02-08 16:09:04 +0700

    Update PID_Speed.ino

--- PID_Speed/PID_Speed.ino
+++ PID_Speed/PID_Speed.ino
@@ -1,16 +1,20 @@
 #define interruptPin  2
+#define GreenLED  3
+#define RedLED  4
 #define RPWM 9
 #define LPWM 10
 
 String mySt = "";
-
 boolean stringComplete = false;  // whether the string is complete
 boolean motor_start = false;
 boolean m_direction = true;
 
+int tdelay = 50;
+
 const int tickmark = 1;
 const int rpm_max = 2500;
 unsigned long prevmillis = 0; // To store time
+unsigned long duration; // To store time difference
 
 double rpm;
 
@@ -25,13 +29,18 @@ void setup() {
     // put your setup code here, to run once:
     pinMode(RPWM,OUTPUT);
     pinMode(LPWM,OUTPUT);
+    pinMode(GreenLED,OUTPUT);
+    pinMode(RedLED,OUTPUT);
 
     digitalWrite(RPWM,LOW);
     digitalWrite(LPWM,LOW);
+    digitalWrite(GreenLED,LOW);
+    digitalWrite(RedLED,HIGH);
     
     delay(1000);
     
     Serial.begin(115200);
+    Serial3.begin(115200);
 
     while (!Serial) {
       ; // wait for serial port to connect. Needed for native USB port only
@@ -64,9 +73,11 @@ void loop() {
       }
       else if (mySt.substring(0,5) == "start"){
         motor_start = true;
+        digitalWrite(13,HIGH);
       }
       else if (mySt.substring(0,4) == "stop"){
         motor_start = false;
+        digitalWrite(13,LOW);
       }
       else if (mySt.substring(0,10) == "turn_right"){
         m_direction =  true;
@@ -86,12 +97,14 @@ ISR(TIMER1_COMPA_vect){          // timer compare interrupt service routine
     double ITerm;
     double DTerm;
     double PIDTerm;
-    
+
     static double e_speed_pre = 0; // posisi lama
     static double e_speed_buff = 0; // posisi baru
     static double e_speed = 0; // perbedaan feedback dengan input
     static double e_speed_sum = 0; // hasil integral error
     static double pwm_pulse = 0;
+
+    static int counter=0;
     
     if ((micros() - prevmillis) > (60000000/tickmark)){
       rpm = 0;
@@ -126,6 +139,8 @@ ISR(TIMER1_COMPA_vect){          // timer compare interrupt service routine
         e_speed_buff = 0;
         e_speed_sum = 0;
         pwm_pulse = 0;
+        prevmillis = 0;
+        duration = 0;
         Serial.print("stop\t");
     }
 
@@ -141,15 +156,20 @@ ISR(TIMER1_COMPA_vect){          // timer compare interrupt service routine
       analogWrite(LPWM, pwm_pulse);
       Serial.print("left\t");
     }
-      
+
+    if (counter>=tdelay){
+      //Serial3.println();
+      Serial3.println(m_speed);
+      counter=0;
+    }
     Serial.print(m_speed);
     Serial.print("\t");
     Serial.println(set_speed);
+    counter++;
 }
 
 void RPM() {
     boolean currentstate = digitalRead(interruptPin); // Current state of Proximity input scan
-    unsigned long duration; // To store time difference
     
     if( currentstate == LOW ) // If input only changes from HIGH to LOW
     {
@@ -174,6 +194,24 @@ void serialEvent() {
     // so the main loop can do something about it:
     if (inChar == '\n') {
       stringComplete = true;
+    }
+  }
+}
+
+void serialEvent3() {
+  while (Serial3.available()) {
+    // get the new byte:
+    char inChar = (char)Serial3.read();
+    // add it to the inputString:
+    if (inChar != '\n') {
+      digitalWrite(GreenLED,HIGH);
+      mySt += inChar;
+    }
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+      digitalWrite(GreenLED,LOW);
     }
   }
 }
